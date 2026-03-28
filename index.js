fd5fc27888380c3f343a65e213e1c0b1c06c173c
emi-8086 <or715.22.a.s@gmail.com>
2026-03-27 18:27:36 -0600

    libreria para generar hashes y prueba de minado local

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.listen(PORT, () => {
     console.log(`Nodo de la Blockchain iniciado en http://localhost:${PORT}`);
 });
 
-const Blockchain = require('/src/models/Blockchain');
+const Blockchain = require('./src/models/Blockchain');
 const nodoAcademico = new Blockchain(); 
 
 app.post('/transactions', (req, res) => {
@@ -33,5 +33,28 @@ app.post('/transactions', (req, res) => {
         success: true,
         message: "Transacción recibida y guardada en la lista de pendientes local.",
         transaccionesPendientes: nodoAcademico.pendingTransactions
+    });
+});
+
+app.post('/mine', (req, res) => {
+    if (nodoAcademico.pendingTransactions.length === 0) {
+        return res.status(400).json({ error: "No hay transacciones pendientes para minar." });
+    }
+
+    const currentBlockData = nodoAcademico.pendingTransactions[0];
+    // obtiene el hash del bloque anterior
+    const lastBlock = nodoAcademico.getLastBlock();
+    const previousBlockHash = lastBlock.hash_actual;
+    // ejecuta Proof of Work para encontrar el nonce
+    const nonce = nodoAcademico.proofOfWork(previousBlockHash, currentBlockData);
+    // genera el hash final
+    const blockHash = nodoAcademico.hashBlock(previousBlockHash, currentBlockData, nonce);
+    // crear el bloque en mi cadena local
+    const newBlock = nodoAcademico.createNewBlock(nonce, previousBlockHash, blockHash, currentBlockData);
+
+    res.status(200).json({
+        success: true,
+        message: "Bloque minado y añadido a la cadena local exitosamente",
+        block: newBlock
     });
 });
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -1,14 +1,57 @@
+const sha256 = require('crypto-js/sha256');
+
 class Blockchain {
     constructor() {
-        this.chain = []; //cadena de bloques local 
-        this.pendingTransactions = []; // transacciones en espera de ser minadas 
-        this.networkNodes = []; // URLs de los nodos 
+        this.chain = []; 
+        this.pendingTransactions = []; 
+        this.networkNodes = []; 
+        
+        // Crear el primer bloque de la red con hash
+        this.createNewBlock(0, '0', '0', {}); 
     }
 
-    // guardar la transacción localmente 
     createNewTransaction(transactionData) {
         this.pendingTransactions.push(transactionData);
         return this.pendingTransactions;
+    }
+
+    // Método para crear el bloque y añadirlo a la cadena local
+    createNewBlock(nonce, previousBlockHash, hash, degreeData) {
+        const newBlock = {
+            index: this.chain.length + 1,
+            timestamp: Date.now(),
+            datos_grado: degreeData, // Los datos de la transacción
+            nonce: nonce,
+            hash_actual: hash,
+            hash_anterior: previousBlockHash,
+        };
+
+        this.pendingTransactions = []; // limpia las transacciones pendientes
+        this.chain.push(newBlock);
+        return newBlock;
+    }
+
+    getLastBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    // Generar el Hash 
+    hashBlock(previousBlockHash, currentBlockData, nonce) {
+        const dataAsString = `${currentBlockData.persona_id}${currentBlockData.institucion_id}${currentBlockData.titulo_obtenido}${currentBlockData.fecha_fin}${previousBlockHash}${nonce}`;
+        return sha256(dataAsString).toString();
+    }
+
+    //buscar el nonce correcto
+    proofOfWork(previousBlockHash, currentBlockData) {
+        let nonce = 0;
+        let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+        
+        while (hash.substring(0, 3) !== '000') {
+            nonce++;
+            hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+        }
+        
+        return nonce;
     }
 }
 
